17d0af2f4c28d2499e21874399c51f76b70612ae
MauriceToast <support@pgt-technologies.com>
2026-03-11 15:52:05 +0100

    Update NL_Playoffs_Selenium.py

--- NL_Playoffs_Selenium.py
+++ NL_Playoffs_Selenium.py
@@ -306,7 +306,7 @@ try:
         all_matches_data.extend(matches_data)
     
     if all_matches_data:
-        write_matches_to_csv(all_matches_data, 'nl_matches.csv')
+        write_matches_to_csv(all_matches_data, 'nl__playoffs_matches.csv')
         print(f"Total matches written to CSV: {len(all_matches_data)}")
     else:
         print("No data collected for any month")
